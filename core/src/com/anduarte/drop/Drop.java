7f9c620bf8c455242e0aa506efa4f989bdbb43c1
Andre Duarte <andreduarte13@hotmail.com>
2016-06-11 12:33:49 +0100

    Rendering the bucket

--- core/src/com/anduarte/drop/Drop.java
+++ core/src/com/anduarte/drop/Drop.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Drop extends ApplicationAdapter {
 	// load the assets and store a reference to them
@@ -18,6 +19,8 @@ public class Drop extends ApplicationAdapter {
 
     private OrthographicCamera camera;
     private SpriteBatch batch;			// Special class used to draw 2D Images
+
+    private Rectangle bucket;
 
 	@Override
 	public void create () {
@@ -38,11 +41,29 @@ public class Drop extends ApplicationAdapter {
         camera.setToOrtho(false, 800, 480);
 
         batch = new SpriteBatch();
+
+        bucket = new Rectangle();
+        bucket.x = 800 / 2 - 64 / 2;    // Position in the middle of the screen
+        bucket.y = 20;                  // Position bottom of the screen
+        bucket.width = 64;
+        bucket.height = 64;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);         // Clear the screen with dark blue color
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // Cameras are responsible for setting up coordinate system for rendering
+        // camera.update(); // Good practice to update camera once per frame (Don´t need in this game
+
+        // Rendering the bucket
+        batch.setProjectionMatrix(camera.combined); // Tells to SpriteBatch to use the coordinate system specified by the camera
+
+        // SpriteBatch class will record all drawing commands in between begin and end
+        // Once we call the end() it will submit all drawing requests at once to OpenGL
+        batch.begin();
+        batch.draw(bucketImage, bucket.x, bucket.y);
+        batch.end();
 	}
 }
